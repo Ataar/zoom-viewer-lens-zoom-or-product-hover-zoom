e9597267e12a8878cb3b5f38e8d1d631398f91b9
Ataar <mateenataar07@gmail.com>
2025-07-15 20:33:17 +0530

    first commit

--- src/app/shared/components/even-odd/even-odd.component.ts
+++ src/app/shared/components/even-odd/even-odd.component.ts
@@ -5,34 +5,27 @@ import { Of2Service } from '../../services/of2.service';
 @Component({
   selector: 'app-even-odd',
   templateUrl: './even-odd.component.html',
-  styleUrls: ['./even-odd.component.scss']
+  styleUrls: ['./even-odd.component.scss'],
 })
-export class EvenOddComponent implements OnInit , OnDestroy {
- 
- data : number[]=[];
+export class EvenOddComponent implements OnInit, OnDestroy {
+  data: number[] = [];
 
-unSubscribe !: Subscription
-  constructor(
-   private f : Of2Service) { }
+  unSubscribe!: Subscription;
+
+  constructor(private f: Of2Service) {}
+
   ngOnInit(): void {
-   
-    this.unSubscribe = this.f.getNum().subscribe(res => {
-  if (res % 2 === 0) {
-    // console.log('Even number received:', res);
-    this.data.push(res);
+    this.unSubscribe = this.f.getNum().subscribe((res) => {
+      if (res % 2 === 0) {
+        // console.log('Even number received:', res);
+        this.data.push(res);
+      }
+    });
+
+    //  console.log(this.data)
   }
-});
 
-
-       
-
-      
-
-      //  console.log(this.data)
-       
-  }
-  
   ngOnDestroy(): void {
-    this.unSubscribe.unsubscribe()
+    this.unSubscribe.unsubscribe();
   }
 }
