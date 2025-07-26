97d6f5bec74023e21f0652d1d26f9b7d23902c87
Ataar <mateenataar07@gmail.com>
2025-07-26 12:36:26 +0530

    first commit

--- src/app/shared/components/todo-items/todo-items.component.ts
+++ src/app/shared/components/todo-items/todo-items.component.ts
@@ -1,4 +1,44 @@
+
+// 📦 Import core Angular functionality
+import { Component, OnInit } from '@angular/core';
+
+// 📦 Import the custom Todo service to fetch todo data
+import { TodoServiceService } from '../../services/todo-service.service';
+
+// 📦 Import the Todo array type or interface
+import { todoArr } from '../../module/todo';
+
+// 🧩 Component decorator to define selector, template, and styles
+@Component({
+  selector: 'app-todo-items',                  // 🔹 Custom HTML tag used for this component
+  templateUrl: './todo-items.component.html',  // 🔹 HTML template for this component
+  styleUrls: ['./todo-items.component.scss']   // 🔹 SCSS styling specific to this component
+})
+export class TodoItemsComponent implements OnInit {
+
+  // 📌 Array to store todo items retrieved from the service
+  todoData: todoArr[] = [];
+
+  // 🔧 Injecting the Todo service using Angular's dependency injection
+  constructor(private todoService: TodoServiceService) {}
+
+  // 🚀 Lifecycle hook called once the component is initialized
+  ngOnInit(): void {
+    // 📥 Fetch todos from the service and store in `todoData`
+    this.todoData = this.todoService.getTodos();
+  }
+}
+
+
+
+
+
+
+
+
 // import { Component, OnInit } from '@angular/core';
+// import { TodoServiceService } from '../../services/todo-service.service';
+// import { todoArr } from '../../module/todo';
 
 // @Component({
 //   selector: 'app-todo-items',
@@ -6,30 +46,11 @@
 //   styleUrls: ['./todo-items.component.scss']
 // })
 // export class TodoItemsComponent implements OnInit {
+//   todoData: todoArr[] = [];
 
-//   constructor() { }
+//   constructor(private todoService: TodoServiceService) {}
 
 //   ngOnInit(): void {
+//     this.todoData = this.todoService.getTodos();
 //   }
-
 // }
-// src/app/components/todo-items/todo-items.component.ts
-
-import { Component, OnInit } from '@angular/core';
-import { TodoServiceService } from '../../services/todo-service.service';
-import { todoArr } from '../../module/todo';
-
-@Component({
-  selector: 'app-todo-items',
-  templateUrl: './todo-items.component.html',
-  styleUrls: ['./todo-items.component.scss']
-})
-export class TodoItemsComponent implements OnInit {
-  todoData: todoArr[] = [];
-
-  constructor(private todoService: TodoServiceService) {}
-
-  ngOnInit(): void {
-    this.todoData = this.todoService.getTodos();
-  }
-}
