da76706d9d9e03166c986648981d734f14e40bdf
Ataar <mateenataar07@gmail.com>
2025-08-01 21:46:53 +0530

    Initial push to navbar-code Bitbucket repo

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,26 +1,3 @@
-// import { Component, OnInit, AfterViewInit, Renderer2, OnDestroy } from '@angular/core';
-
-
-// @Component({
-//   selector: 'app-dashboard',
-//   templateUrl: './dashboard.component.html',
-//   styleUrls: ['./dashboard.component.scss']
-// })
-// export class DashboardComponent implements OnInit {
-// isDarkMode = false;
-
-
-// constructor() {}
-
-// ngOnInit(): void {
-// }
-
-// toggleTheme() {
-//   this.isDarkMode = !this.isDarkMode;
-// }
-// }
-
-
 // 📦 Import required Angular core modules
 import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 
